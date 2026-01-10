b34cebc740cd6fac6f5ba0ed1c746aeb37af8b46
Charles Severance <csev@umich.edu>
2026-01-10 18:32:04 -0500

    More icon awesomeness

--- lib/util.php
+++ lib/util.php
@@ -80,6 +80,12 @@ function headerContent($headCSS=false) {
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" >
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo($CFG->servicename); ?></title>
+    <!-- Favicons -->
+    <link rel="icon" type="image/x-icon" href="<?php echo($CFG->wwwroot); ?>/favicon.ico">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo($CFG->wwwroot); ?>/favicon_io/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo($CFG->wwwroot); ?>/favicon_io/favicon-32x32.png">
+    <link rel="apple-touch-icon" href="<?php echo($CFG->wwwroot); ?>/favicon_io/apple-touch-icon.png">
+    <link rel="manifest" href="<?php echo($CFG->wwwroot); ?>/favicon_io/site.webmanifest">
     <!-- Le styles -->
     <link href="<?php echo($CFG->staticroot); ?>/static/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo($CFG->staticroot); ?>/static/bootstrap-theme.min.css" rel="stylesheet">
